7baa66593c6cb1953f6956342fc17371f59aa815
Isya-25th <isyaputra.r25th@gmail.com>
2025-11-27 11:11:46 +0700

    Percobaan 4

---
+++ PengunjungCafe13.java
@@ -0,0 +1,13 @@
+public class PengunjungCafe13 {
+    static void daftarPengunjung(String...namaPengunjung) {
+        System.out.println("Daftar Nama Pengunjung: ");
+        for (int i = 0; i < namaPengunjung.length; i++) {
+            System.out.println("- " + namaPengunjung);
+        }
+    }
+    public static void main(String[] args) {
+        daftarPengunjung("Ali", "Budi", "Citra");
+        daftarPengunjung("Andi");
+        daftarPengunjung("Doni", "Eti", "Fahmi", "Galih");
+    }
+}
