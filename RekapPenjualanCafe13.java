9408eda57f657b7fe465d7d5413839a0365b8530
Isya-25th <isyaputra.r25th@gmail.com>
2025-11-27 11:55:35 +0700

    Modifikasi Tugas 3

--- RekapPenjualanCafe13.java
+++ RekapPenjualanCafe13.java
@@ -3,13 +3,20 @@ import java.util.Scanner;
 public class RekapPenjualanCafe13 {
     static Scanner sc = new Scanner(System.in);
     static String[] namaMenu = {"Kopi", "Teh", "Es Kelapa Muda", "Roti Bakar", "Gorengan"};
-    static int[][] dataPenjualan = new int[5][7];
+    static int[][] dataPenjualan;
+    static int jumlahHari;
+
+    public static void inputJumlahHari() {
+        System.out.print("Masukkan jumlah hari penjualan: ");
+        jumlahHari = sc.nextInt();
+        dataPenjualan = new int[namaMenu.length][jumlahHari];
+    }
 
     public static void inputDataPenjualan() {
-        System.out.println("Input Data Penjualan: ");
-        for (int i = 0; i < dataPenjualan.length; i++) {
+        System.out.println("\nInput Data Penjualan: ");
+        for (int i = 0; i < namaMenu.length; i++) {
             System.out.println("Input penjualan untuk " + namaMenu[i] + ":");
-            for (int j = 0; j < dataPenjualan[0].length; j++) {
+            for (int j = 0; j < jumlahHari; j++) {
                 System.out.print("Hari ke-" + (j + 1) + ": ");
                 dataPenjualan[i][j] = sc.nextInt();
             }
@@ -18,16 +25,16 @@ public class RekapPenjualanCafe13 {
     }
 
     public static void tampilkanTabel() {
-        System.out.println("Tabel Penjualan Selama 7 Hari:");
+        System.out.println("\nTabel Penjualan Selama " + jumlahHari + " Hari:");
         System.out.print("Menu\t\t");
-        for (int i = 1; i <= 7; i++) {
+        for (int i = 1; i <= jumlahHari; i++) {
             System.out.print("H" + i + "\t");
         }
         System.out.println();
-        
-        for (int i = 0; i < dataPenjualan.length; i++) {
-            System.out.printf("%-15s\t", namaMenu[i]); 
-            for (int j = 0; j < dataPenjualan[0].length; j++) {
+
+        for (int i = 0; i < namaMenu.length; i++) {
+            System.out.printf("%-15s\t", namaMenu[i]);
+            for (int j = 0; j < jumlahHari; j++) {
                 System.out.print(dataPenjualan[i][j] + "\t");
             }
             System.out.println();
@@ -39,9 +46,9 @@ public class RekapPenjualanCafe13 {
         int totalTertinggi = 0;
         String menuTertinggi = "";
 
-        for (int i = 0; i < dataPenjualan.length; i++) {
+        for (int i = 0; i < namaMenu.length; i++) {
             int totalPerMenu = 0;
-            for (int j = 0; j < dataPenjualan[0].length; j++) {
+            for (int j = 0; j < jumlahHari; j++) {
                 totalPerMenu += dataPenjualan[i][j];
             }
 
@@ -50,27 +57,27 @@ public class RekapPenjualanCafe13 {
                 menuTertinggi = namaMenu[i];
             }
         }
-        System.out.println("Menu dengan total penjualan tertinggi adalah: " + menuTertinggi + " (Total: " + totalTertinggi + ")");
+        System.out.println("Menu dengan total penjualan tertinggi adalah: " 
+            + menuTertinggi + " (Total: " + totalTertinggi + ")");
     }
 
     public static void tampilkanRataRata() {
         System.out.println("\nRata-rata Penjualan per Menu:");
-        for (int i = 0; i < dataPenjualan.length; i++) {
+        for (int i = 0; i < namaMenu.length; i++) {
             double total = 0;
-            for (int j = 0; j < dataPenjualan[0].length; j++) {
+            for (int j = 0; j < jumlahHari; j++) {
                 total += dataPenjualan[i][j];
             }
-            double rata = total / dataPenjualan[0].length;
+            double rata = total / jumlahHari;
             System.out.printf("- %s: %.2f\n", namaMenu[i], rata);
         }
     }
+
     public static void main(String[] args) {
-        inputDataPenjualan();
-        
-        tampilkanTabel();
-
-        tampilkanPenjualanTertinggi();
-
-        tampilkanRataRata();
+        inputJumlahHari();      
+        inputDataPenjualan();    
+        tampilkanTabel();   
+        tampilkanPenjualanTertinggi(); 
+        tampilkanRataRata();    
     }
 }
